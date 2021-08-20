b042ca3082a492bd9fdbdded99107e384c8e3055
LucioNessier <nessier.lucio@gmail.com>
2021-08-20 16:22:24 -0300

    Fixed Services Content issue

--- assets/js/main.js
+++ assets/js/main.js
@@ -125,24 +125,24 @@ let swiper = new Swiper('.portfolioContainer', {
 
 /* SCROLL SECTIONS ACTIVE LINK */
 
-const sections = document.querySelectorAll('section[id]')
+// const sections = document.querySelectorAll('section[id]')
 
-function scrollActive(){
-    const scrollY = window.pageYOffset
+// function scrollActive(){
+//     const scrollY = window.pageYOffset
 
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id')
+//     sections.forEach(current =>{
+//         const sectionHeight = current.offsetHeight
+//         const sectionTop = current.offsetTop - 50;
+//         sectionId = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.navMenu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
-            document.querySelector('.navMenu a[href*=' + sectionId + ']').classList.remove('active-link')
-        }
-    })
-}
-window.addEventListener('scroll', scrollActive)
+//         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+//             document.querySelector('.navMenu a[href*=' + sectionId + ']').classList.add('active-link')
+//         }else{
+//             document.querySelector('.navMenu a[href*=' + sectionId + ']').classList.remove('active-link')
+//         }
+//     })
+// }
+// window.addEventListener('scroll', scrollActive)
 
 
 /*CHANGE BACKGROUND HEADER */ 
